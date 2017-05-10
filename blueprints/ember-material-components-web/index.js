0f2e49098e6712715ea5340b34f33d8c424dec6d
Kerrick Long <me@kerricklong.com>
2017-05-10 10:33:09 -0500

    :arrow_down: Fix dependencies in blueprint as well

--- blueprints/ember-material-components-web/index.js
+++ blueprints/ember-material-components-web/index.js
@@ -2,20 +2,20 @@ module.exports = {
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
   afterInstall: function() {
     return this.addPackagesToProject([
-      { name: '@material/base', target: '^0.1.0' },
-      { name: '@material/animation', target: '^0.1.0' },
-      { name: '@material/theme', target: '^0.1.0' },
-      { name: '@material/checkbox', target: '^0.1.0' },
-      { name: '@material/radio', target: '^0.1.1' },
-      { name: '@material/button', target: '^0.1.1' },
-      { name: '@material/fab', target: '^0.2.0' },
-      { name: '@material/card', target: '^0.1.0' },
-      { name: '@material/icon-toggle', target: '^0.1.1' },
-      { name: '@material/elevation', target: '^0.1.1' },
-      { name: '@material/list', target: '^0.1.0' },
-      { name: '@material/form-field', target: '^0.1.1' },
-      { name: '@material/textfield', target: '^0.2.0' },
-      { name: '@material/menu', target: '^0.1.2' }
+      { name: '@material/base', target: '0.1.0' },
+      { name: '@material/animation', target: '0.1.0' },
+      { name: '@material/theme', target: '0.1.0' },
+      { name: '@material/checkbox', target: '0.1.0' },
+      { name: '@material/radio', target: '0.1.1' },
+      { name: '@material/button', target: '0.1.1' },
+      { name: '@material/fab', target: '0.2.0' },
+      { name: '@material/card', target: '0.1.0' },
+      { name: '@material/icon-toggle', target: '0.1.1' },
+      { name: '@material/elevation', target: '0.1.1' },
+      { name: '@material/list', target: '0.1.0' },
+      { name: '@material/form-field', target: '0.1.1' },
+      { name: '@material/textfield', target: '0.2.0' },
+      { name: '@material/menu', target: '0.1.2' }
     ]);
   }
 };
