83132801eb3bfc18d507acba5a213da0058d9695
Daniel Raggs <daniel.raggs@secondstreet.com>
2017-08-01 12:06:18 -0500

    :hammer: Avoid underscored property names; remove unneeded properties

--- addon/components/mdc-tab-bar-scroller.js
+++ addon/components/mdc-tab-bar-scroller.js
@@ -7,7 +7,7 @@ import getElementProperty from '../utils/get-element-property';
 import getComponentProperty from '../utils/get-component-property';
 import styleComputed from '../utils/style-computed';
 
-const { get, computed, set } = Ember;
+const { get, computed, set, run } = Ember;
 const { cssClasses, strings } = MDCTabBarScrollerFoundation;
 
 export default Ember.Component.extend(MDCComponent, {
@@ -22,10 +22,10 @@ export default Ember.Component.extend(MDCComponent, {
   },
   didInsertElement() {
     this._super(...arguments);
-    set(this, 'scrollFrame_', getElementProperty(this, 'querySelector')(strings.FRAME_SELECTOR));
-    set(this, 'tabBarEl_', getElementProperty(this, 'querySelector')(strings.TABS_SELECTOR));
-    set(this, 'forwardIndicator_', getElementProperty(this, 'querySelector')(strings.INDICATOR_FORWARD_SELECTOR));
-    set(this, 'backIndicator_', getElementProperty(this, 'querySelector')(strings.INDICATOR_BACK_SELECTOR));
+    set(this, 'scrollFrameElement', getElementProperty(this, 'querySelector')(strings.FRAME_SELECTOR));
+    set(this, 'tabBarElement', getElementProperty(this, 'querySelector')(strings.TABS_SELECTOR));
+    set(this, 'forwardIndicatorElement', getElementProperty(this, 'querySelector')(strings.INDICATOR_FORWARD_SELECTOR));
+    set(this, 'backIndicatorElement', getElementProperty(this, 'querySelector')(strings.INDICATOR_BACK_SELECTOR));
   },
   //endregion
 
@@ -38,22 +38,22 @@ export default Ember.Component.extend(MDCComponent, {
   /**
    * @type {Object}
    */
-  scrollFrame_: null,
+  scrollFrameElement: null,
 
   /**
    * @type {Object}
    */
-  tabBarEl_: null,
+  tabBarElement: null,
 
   /**
    * @type {Object}
    */
-  forwardIndicator_: null,
+  forwardIndicatorElement: null,
 
   /**
    * @type {Object}
    */
-  backIndicator_: null,
+  backIndicatorElement: null,
 
   /**
    * @type {String[]}
@@ -80,18 +80,18 @@ export default Ember.Component.extend(MDCComponent, {
   //region Methods
   createFoundation() {
     return new MDCTabBarScrollerFoundation({
-      addClass: (className) => Ember.run(() => get(this, 'mdcClasses').addObject(className)),
-      removeClass: (className) => Ember.run(() => get(this, 'mdcClasses').removeObject(className)),
+      addClass: (className) => run(() => get(this, 'mdcClasses').addObject(className)),
+      removeClass: (className) => run(() => get(this, 'mdcClasses').removeObject(className)),
       eventTargetHasClass: (target, className) => target.classList.contains(className),
-      addClassToForwardIndicator: (className) => Ember.run(() => get(this, 'forwardIndicatorClasses').addObject(className)),
-      removeClassFromForwardIndicator: (className) => Ember.run(() => get(this, 'forwardIndicatorClasses').removeObject(className)),
-      addClassToBackIndicator: (className) => Ember.run(() => get(this, 'backIndicatorClasses').addObject(className)),
-      removeClassFromBackIndicator: (className) => Ember.run(() => get(this, 'backIndicatorClasses').removeObject(className)),
+      addClassToForwardIndicator: (className) => run(() => get(this, 'forwardIndicatorClasses').addObject(className)),
+      removeClassFromForwardIndicator: (className) => run(() => get(this, 'forwardIndicatorClasses').removeObject(className)),
+      addClassToBackIndicator: (className) => run(() => get(this, 'backIndicatorClasses').addObject(className)),
+      removeClassFromBackIndicator: (className) => run(() => get(this, 'backIndicatorClasses').removeObject(className)),
       isRTL: () => getElementProperty(this, 'direction') === 'rtl',
-      registerBackIndicatorClickHandler: (handler) => get(this, 'backIndicator_').addEventListener('click', handler),
-      deregisterBackIndicatorClickHandler: (handler) => get(this, 'backIndicator_').removeEventListener('click', handler),
-      registerForwardIndicatorClickHandler: (handler) => get(this, 'forwardIndicator_').addEventListener('click', handler),
-      deregisterForwardIndicatorClickHandler: (handler) => get(this, 'forwardIndicator_').removeEventListener('click', handler),
+      registerBackIndicatorClickHandler: (handler) => get(this, 'backIndicatorElement').addEventListener('click', handler),
+      deregisterBackIndicatorClickHandler: (handler) => get(this, 'backIndicatorElement').removeEventListener('click', handler),
+      registerForwardIndicatorClickHandler: (handler) => get(this, 'forwardIndicatorElement').addEventListener('click', handler),
+      deregisterForwardIndicatorClickHandler: (handler) => get(this, 'forwardIndicatorElement').removeEventListener('click', handler),
       registerCapturedInteractionHandler: (evt, handler) => get(this, 'element').addEventListener(evt, handler, true),
       deregisterCapturedInteractionHandler: (evt, handler) => get(this, 'element').removeEventListener(evt, handler, true),
       registerWindowResizeHandler: (handler) => window.addEventListener('resize', handler),
@@ -99,10 +99,10 @@ export default Ember.Component.extend(MDCComponent, {
       getNumberOfTabs: () => get(this, 'tab-bar.tabs.length'),
       getComputedWidthForTabAtIndex:  (index) => getComponentProperty(get(this, 'tab-bar').tabAt(index), 'computedWidth', 0),
       getComputedLeftForTabAtIndex: (index) => getComponentProperty(get(this, 'tab-bar').tabAt(index), 'computedLeft', 0),
-      getOffsetWidthForScrollFrame: () => get(this, 'scrollFrame_').offsetWidth,
-      getScrollLeftForScrollFrame: () => get(this, 'scrollFrame_').scrollLeft,
-      setScrollLeftForScrollFrame: (scrollLeftAmount) =>  get(this, 'scrollFrame_').scrollLeft = scrollLeftAmount,
-      getOffsetWidthForTabBar: () => get(this, 'tabBarEl_').offsetWidth,
+      getOffsetWidthForScrollFrame: () => get(this, 'scrollFrameElement').offsetWidth,
+      getScrollLeftForScrollFrame: () => get(this, 'scrollFrameElement').scrollLeft,
+      setScrollLeftForScrollFrame: (scrollLeftAmount) =>  get(this, 'scrollFrameElement').scrollLeft = scrollLeftAmount,
+      getOffsetWidthForTabBar: () => get(this, 'tabBarElement').offsetWidth,
       setTransformStyleForTabBar: (value) => {
         this.setStyleFor('mdcScrollFrameStyles', getCorrectPropertyName(window, 'transform'), value);
       },
@@ -113,8 +113,6 @@ export default Ember.Component.extend(MDCComponent, {
   //endregion
 
   //region Computed Properties
-  isIconsOnly: computed.equal('icons', 'only'),
-  isIconsWithText: computed.equal('icons', 'with-text'),
   scrollFrameStyles: styleComputed('mdcScrollFrameStyles'),
   /**
    * @returns {String}
